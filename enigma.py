d714957664160a2c34813da7ba775422cbeafded
pat <scyzz1@nottingham.edu.cn>
2020-01-15 20:05:38 +0800

    Update the files
    It is a real enigma now

--- enigma.py
+++ enigma.py
@@ -9,27 +9,58 @@ from random import shuffle
 import warnings
 warnings.filterwarnings("ignore")
 
+def roll_the_roter(roter):
+    for i in range(len(roter)):
+        if(roter[i] == 94):
+            roter[i] = 0
+        else:
+            roter[i] = roter[i] + 1
+
+
+
 ifreset = int(input("Reset the code? (type 0 for not reset, otherwise reset): "))
 
 if(ifreset != 0):
     print("Reset the code...")
-    mt_password = [i for i in range(95)]
-    mt_password = np.array(mt_password)
-    shuffle(mt_password)
+    
+    mt_password1 = [i for i in range(95)]
+    mt_password1 = np.array(mt_password1)
+    shuffle(mt_password1)
+    
+    mt_password2 = [i for i in range(95)]
+    mt_password2 = np.array(mt_password2)
+    shuffle(mt_password2)
+    
+    mt_password3 = [i for i in range(95)]
+    mt_password3 = np.array(mt_password3)
+    shuffle(mt_password3)
+    
+    """
     real_tran_table = np.zeros((95),dtype=np.int)
-    
     for i in range(95):
-        num_fr = mt_password[i]
-        num_se = mt_password[num_fr]
-        num_thr = mt_password[num_se]
+        num_fr = mt_password1[i]
+        num_se = mt_password2[num_fr]
+        num_thr = mt_password3[num_se]
         real_tran_table[i] = mt_password[num_thr];
+    """
     
-    np.savetxt("real_tran_table.txt", real_tran_table,fmt='%d',delimiter=',')
+    np.savetxt("roter1.txt", mt_password1,fmt='%d',delimiter=',')
+    np.savetxt("roter2.txt", mt_password2,fmt='%d',delimiter=',')
+    np.savetxt("roter3.txt", mt_password3,fmt='%d',delimiter=',')
 else:
     print("Use the original code")
-    real_tran_table = np.loadtxt('real_tran_table.txt',delimiter=',')
-    real_tran_table = np.array(real_tran_table)
-    real_tran_table = real_tran_table.astype(np.int32)
+    
+    mt_password1 = np.loadtxt('roter1.txt',delimiter=',')
+    mt_password1 = np.array(mt_password1)
+    mt_password1 = mt_password1.astype(np.int32)
+    
+    mt_password2 = np.loadtxt('roter2.txt',delimiter=',')
+    mt_password2 = np.array(mt_password2)
+    mt_password2 = mt_password2.astype(np.int32)
+    
+    mt_password3 = np.loadtxt('roter3.txt',delimiter=',')
+    mt_password3 = np.array(mt_password3)
+    mt_password3 = mt_password3.astype(np.int32)
     
 file = open("input_article.txt","r")
 string_input = file.read()
@@ -38,10 +69,28 @@ file.close()
 ascii_code = np.fromstring(string_input, dtype=np.uint8)
 op_array = np.zeros((len(ascii_code)),dtype=np.int)
 
+fr_caler = 0
+se_caler = 0
+thr_caler = 0
+
 for i in range(len(ascii_code)):
     if(32 <= ascii_code[i] <= 126):
         now_code = ascii_code[i] - 32
-        op_array[i] = real_tran_table[now_code] + 32
+        num_fr = mt_password1[now_code]
+        num_se = mt_password2[num_fr]
+        op_array[i] = mt_password3[num_se] + 32
+        fr_caler = fr_caler + 1
+        roll_the_roter(mt_password1)
+        if(fr_caler == 95):
+            fr_caler = 0
+            se_caler = se_caler + 1
+            roll_the_roter(mt_password2)
+        if(se_caler == 95):
+            se_caler = 0
+            thr_caler = thr_caler + 1
+            roll_the_roter(mt_password3)
+        if(thr_caler == 95):
+            thr_caler = 0
     else:
         op_array[i] = ascii_code[i]
         
--- enigma_translator.py
+++ enigma_translator.py
@@ -6,25 +6,62 @@ Created on Tue Jan 14 11:17:11 2020
 """
 import numpy as np
 
-#turing's bombe
+def roll_the_roter(roter):
+    for i in range(len(roter)):
+        if(roter[i] == 94):
+            roter[i] = 0
+        else:
+            roter[i] = roter[i] + 1
 
 #decode
 file = open("encrypted_message.txt","r")
 op_string = file.read()
 file.close()
 #op_string = input("Encrypted message: ")
-readed_password = np.loadtxt('real_tran_table.txt',delimiter=',')
-readed_password = np.array(readed_password)
-readed_password = readed_password.astype(np.int32)
-back_search_table = np.zeros((95),dtype=np.int)
-for i in range(95):
-    back_search_table[readed_password[i]] = i
-    
+readed_password1 = np.loadtxt('roter1.txt',delimiter=',')
+readed_password1 = np.array(readed_password1)
+readed_password1 = readed_password1.astype(np.int32)
+
+readed_password2 = np.loadtxt('roter2.txt',delimiter=',')
+readed_password2 = np.array(readed_password2)
+readed_password2 = readed_password2.astype(np.int32)
+
+readed_password3 = np.loadtxt('roter3.txt',delimiter=',')
+readed_password3 = np.array(readed_password3)
+readed_password3 = readed_password3.astype(np.int32)
+
+idx_list1 = np.argsort(readed_password1)
+idx_list2 = np.argsort(readed_password2)
+idx_list3 = np.argsort(readed_password3)
+
+fr_caler = 0
+se_caler = 0
+thr_caler = 0
+
 decode_op = ''
 decode_array = np.fromstring(op_string, dtype=np.uint8)
+
 for i in range(len(decode_array)):
     if(32 <= decode_array[i] <= 126):
-        decode_op = decode_op + chr(back_search_table[decode_array[i] - 32] + 32)
+        now_code = decode_array[i] - 32
+        num_fr = idx_list3[now_code]
+        num_se = idx_list2[num_fr]
+        decode_op = decode_op + chr(idx_list1[num_se] + 32)
+        fr_caler = fr_caler + 1
+        roll_the_roter(readed_password1)
+        idx_list1 = np.argsort(readed_password1)
+        if(fr_caler == 95):
+            fr_caler = 0
+            se_caler = se_caler + 1
+            roll_the_roter(readed_password2)
+            idx_list2 = np.argsort(readed_password2)
+        if(se_caler == 95):
+            se_caler = 0
+            thr_caler = thr_caler + 1
+            roll_the_roter(readed_password3)
+            idx_list3 = np.argsort(readed_password3)
+        if(thr_caler == 95):
+            thr_caler = 0
     else:
         decode_op = decode_op + chr(decode_array[i])
 
